2883e17438bb7b9a693f2093afe80a37edeb6368
Adam Djo <adambenmessaoud2@gmail.com>
2023-11-26 15:43:48 +0100

    Authentificaiton configuration

--- src/feature/layout/Footer.tsx
+++ src/feature/layout/Footer.tsx
@@ -1,17 +1,48 @@
-import { HomeIcon } from 'lucide-react';
+import { buttonVariants } from '@/components/ui/button';
+import clsx from 'clsx';
+import { Home, PenSquare, User } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="py-2    fixed w-full gap-1  bottom-0  border-t border-accent ">
-      <div className="container">
-        <Link href="/">
-          <HomeIcon size={16}></HomeIcon>
+    <footer className="py-2  fixed w-full gap-5  bottom-0  border-t border-t-accent ">
+      <div className="container flex justify-between">
+        <Link
+          href="/"
+          className={clsx(
+            buttonVariants({
+              variant: 'ghost',
+            }),
+            'flex-1'
+          )}
+        >
+          <Home size={16}></Home>
+        </Link>
+        <Link
+          href="/write"
+          className={clsx(
+            buttonVariants({
+              variant: 'ghost',
+            }),
+            'flex-1'
+          )}
+        >
+          <PenSquare size={16}></PenSquare>
+        </Link>
+        <Link
+          href="/profile"
+          className={clsx(
+            buttonVariants({
+              variant: 'ghost',
+            }),
+            'flex-1'
+          )}
+        >
+          <User size={16}></User>
         </Link>
       </div>
     </footer>
   );
 };
-console.log('aaaaaaaaaaa');
 export default Footer;
